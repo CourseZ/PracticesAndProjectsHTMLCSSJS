99a37951650c30e806c17316fb30b55cdac192ca
CourseZ <nguyenvanlong17052002@gmail.com>
2023-03-28 21:38:25 +0700

    10 Background Image Scroll Image v1.0

---
+++ 10BackgroundImageScrollEffect/index.js
@@ -0,0 +1,10 @@
+const bgImageElem = document.getElementById('bg-image');
+
+window.addEventListener('scroll', () => {
+    updateImage();
+});
+
+function updateImage() {
+    bgImageElem.style.opacity = 1 - window.pageYOffset / 900;
+    bgImageElem.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+}
